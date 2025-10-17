bdc8505b2458fc094bf1a396738c49fe33b212ea
pwlgk <pawelgeyko@gmail.com>
2025-10-17 13:34:24 +0600

    Add versions to docs

--- app/routers/v1/api.py
+++ app/routers/v1/api.py
@@ -10,15 +10,15 @@ from app.routers.v1.endpoints import admin as admin_v1_router
 api_router = APIRouter(prefix="/v1")
 
 # Пользовательские и публичные эндпоинты
-api_router.include_router(auth.router, tags=["Authentication"])
-api_router.include_router(user.router, tags=["Users"])
-api_router.include_router(catalog.router, tags=["Catalog"])
-api_router.include_router(cart.router, tags=["Cart & Favorites"])
-api_router.include_router(order.router, tags=["Orders"])
-api_router.include_router(settings.router, tags=["Settings"])
-api_router.include_router(coupon.router, tags=["Coupons"])
-api_router.include_router(notification.router, tags=["Notifications"])
-api_router.include_router(cms.router, tags=["CMS"])
+api_router.include_router(auth.router, tags=["Authentication V_1"])
+api_router.include_router(user.router, tags=["Users V_1"])
+api_router.include_router(catalog.router, tags=["Catalog V_1"])
+api_router.include_router(cart.router, tags=["Cart & Favorites V_1"])
+api_router.include_router(order.router, tags=["Orders V_1"])
+api_router.include_router(settings.router, tags=["Settings V_1"])
+api_router.include_router(coupon.router, tags=["Coupons V_1"])
+api_router.include_router(notification.router, tags=["Notifications V_1"])
+api_router.include_router(cms.router, tags=["CMS V_1"])
 
 # Админские эндпоинты
-api_router.include_router(admin_v1_router.router, prefix="/admin", tags=["Admin"])
+api_router.include_router(admin_v1_router.router, prefix="/admin", tags=["Admin V_1"])
